3b96253ad7ec88f082b492c43e004e13897aa889
浮焕然 <hr328030@mail.ustc.edu.cn>
2025-04-22 15:30:20 +0800

    Add files via upload
    这个版本尝试了我在实验室一个横向项目中的应用
    但是只是调用了一个深度学习模型跑测试，没有涉及requests库因此只是尝试

---
+++ project_fhr_v3/button_function.py
@@ -0,0 +1,89 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk  # 需要安装Pillow库
+from tkinter import filedialog
+from function_else import ask_adddress
+from tracking.track import run__
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Tkinter 示例")
+        self.root.geometry("640x640")  # 设置窗口大小
+        self.load_main_directory()
+
+    def load_main_directory(self):
+        # 清空当前窗口的所有内容
+        self.clear_widgets()
+        
+        # 添加一个标签
+        label = tk.Label(self.root, text="这是主目录！", font=("Arial", 14))
+        label.pack(pady=20)
+        
+        # 添加一个按钮，用于进入其他页面
+        other_button = tk.Button(self.root, text="进入其他页面", command=self.go_to_other_page)
+        other_button.pack(pady=10)
+
+        address_button = tk.Button(self.root, text="进入搜索页面", command=self.go_to_input_page)
+        address_button.pack(pady=10)
+    def go_to_other_page(self):
+        # 清空当前窗口的所有内容
+        self.clear_widgets()
+        # 加载其他页面的内容
+        other_label = tk.Label(self.root, text="这是其他页面！", font=("Arial", 14))
+        other_label.pack(pady=20)
+        # 添加一个按钮，用于返回主目录
+        return_button = tk.Button(self.root, text="返回主目录", command=self.load_main_directory)
+        return_button.place(x=0, y=0)
+        
+        # 添加一个按钮，用于选择文件
+        entry = tk.Entry(self.root)
+        entry.pack()
+        address_button = tk.Button(self.root, text="提交", command=lambda: self.run_model(entry=entry))
+        address_button.pack(pady=10)
+        
+
+    def run_model(self,entry):
+        user_input = entry.get()  # 获取输入框的内容
+        print("输入的字符串是：", user_input)
+        run__(user_input)
+        finish_label = tk.Label(self.root, text="完成检测！", font=("Arial", 14))
+        finish_label.pack(pady=20)
+
+    def go_to_input_page(self):
+        # 清空当前窗口的所有内容
+        self.clear_widgets()
+        # 加载其他页面的内容
+        other_label = tk.Label(self.root, text="这是搜索页面！", font=("Arial", 14))
+        other_label.pack(pady=20)
+        # 添加一个按钮，用于返回主目录
+        return_button = tk.Button(self.root, text="返回主目录", command=self.load_main_directory)
+        return_button.place(x=0, y=0)
+        # 添加一个按钮，用于选择文件
+        entry = tk.Entry(self.root)
+        entry.pack()
+        address_button = tk.Button(self.root, text="提交", command=lambda: self.submit(entry=entry))
+        address_button.pack(pady=10)
+        
+        
+
+        
+    def submit(self,entry):
+        
+        user_input = entry.get()  # 获取输入框的内容
+        print("输入的字符串是：", user_input)
+        long_text = ask_adddress(url=user_input)
+        
+        
+        text = tk.Text(self.root, wrap="word", font=("Arial", 14))  # 设置字体和自动换行
+        text.pack(expand=True, fill="both", padx=10, pady=10)  # 填充整个窗口
+        scrollbar = ttk.Scrollbar(self.root, command=text.yview)
+        scrollbar.pack(side="right", fill="y")
+        text.config(yscrollcommand=scrollbar.set)
+
+        # 设置要显示的长文本
+        text.insert("1.0", long_text)  # 插入文本到 Text 控件
+        
+    def clear_widgets(self):
+        # 清空当前窗口的所有内容
+        for widget in self.root.winfo_children():
+            widget.destroy()
---
+++ project_fhr_v3/function_else.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+url = 'https://news.ustc.edu.cn/info/1155/89289.htm'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)\
+Chrome/55.0.2883.87 Safari/537.36'}
+
+
+def ask_adddress(url='https://news.ustc.edu.cn/info/1155/89289.htm',headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)\
+Chrome/55.0.2883.87 Safari/537.36'}):
+    r = requests.get(url, headers = headers)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    text_content = soup.get_text()
+    return text_content
